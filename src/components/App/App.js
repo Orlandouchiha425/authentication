b3c734f385a4fc4a7145ae48176576259b6d2356
orlandouchiha425 <valadez425@gmail.com>
2023-04-25 04:51:26 -0400

    major clean up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,9 @@
-import React, { useState } from "react";
-import SignUpForm from "../SignUpForm/SignUpForm";
-import LoginForm from "../LoginForm/LoginForm";
-import { getUser } from "../../utilities/users-service";
-import { Routes, Route } from "react-router-dom";
-import UserLogOut from "../Logout/Logout";
-import Home from "../Home/Home";
+import CleanUp from "../CleanUp/CleanUp";
 
 function App() {
-  const [user, setUser] = useState(getUser());
-
   return (
     <div className="App">
-      <Routes>
-        {user ? (
-          <>
-            <Route path="/home" element={<Home setUser={setUser} />} />
-            <Route path='/logout' element = {<UserLogOut user={user} setUser={setUser}/>}/>
-
-          </>
-        ) : (
-          <>
-            <Route
-              path="/signup"
-              element={<SignUpForm setUser={setUser} user={user} />}
-            />
-            <Route
-              path="/login"
-              element={<LoginForm setUser={setUser} user={user} />}
-            />
-              <Route path="/*"  element={<SignUpForm  setUser={setUser} user={user}/>}/>
-
-          </>
-        )}
-      </Routes>
+      <CleanUp />
     </div>
   );
 }
--- src/components/CleanUp/CleanUp.jsx
+++ src/components/CleanUp/CleanUp.jsx
@@ -1,35 +1,42 @@
-// import React, { useState } from "react";
-// import SignUpForm from "../SignUpForm/SignUpForm";
-// import { getUser } from "../../utilities/users-service";
-// import Home from "../Home/Home";
-// import { Routes, Route } from "react-router-dom";
-
-// function CleanUp() {
-//   const [user, setUser] = useState(getUser());
-
-//   return (
-//     <div>
-//         { user ?
-//         <>
-//           <Routes>
-//             <Route path="/home" element={<Home user={user} setUser={setUser} />} />
-         
-//           </Routes>
-//         </>
-//         :
-//         <SignUpForm setUser={setUser} />
-//       }
-//     </div>
-//   );
-// }
-
-// export default CleanUp;
-import React from 'react'
+import React, { useState } from "react";
+import SignUpForm from "../SignUpForm/SignUpForm";
+import { getUser } from "../../utilities/users-service";
+import Home from "../Home/Home";
+import { Routes, Route } from "react-router-dom";
+import UserLogOut from "../Logout/Logout";
+import LoginForm from "../LoginForm/LoginForm";
 
 function CleanUp() {
+  const [user, setUser] = useState(getUser());
+
   return (
-    <div>CleanUp</div>
-  )
+    <Routes>
+      {user ? (
+        <>
+          <Route path="/home" element={<Home setUser={setUser} />} />
+          <Route
+            path="/logout"
+            element={<UserLogOut user={user} setUser={setUser} />}
+          />
+        </>
+      ) : (
+        <>
+          <Route
+            path="/signup"
+            element={<SignUpForm setUser={setUser} user={user} />}
+          />
+          <Route
+            path="/login"
+            element={<LoginForm setUser={setUser} user={user} />}
+          />
+          <Route
+            path="/*"
+            element={<SignUpForm setUser={setUser} user={user} />}
+          />
+        </>
+      )}
+    </Routes>
+  );
 }
 
-export default CleanUp
+export default CleanUp;
